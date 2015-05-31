ec56c8c4f1ff8b9a9101d2976c8d4b6a4e00d0ed
AndresOtero <oteroandres95@gmail.com>
2015-05-30 22:54:40 -0300

    Primer commit

---
+++ algocraft/test/algo3/algocraft/JuegoTest.java
@@ -0,0 +1,16 @@
+package algo3.algocraft;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+@SuppressWarnings("unused")
+public class JuegoTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(2, 2);
+	}
+
+}
