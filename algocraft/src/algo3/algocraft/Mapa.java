50027291aef49e297f89c0f90a9b88a1115ff587
alan <alanbenju@hotmail.com>
2015-06-04 01:30:43 -0300

    Mapa-serparado edificios de gas y mineral

--- algocraft/src/algo3/algocraft/Mapa.java
+++ algocraft/src/algo3/algocraft/Mapa.java
@@ -3,6 +3,8 @@ package algo3.algocraft;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import algo3.algocraft.edificios.EdificioDeRecurso;
 import algo3.algocraft.exceptions.*;
 
 
@@ -11,13 +13,18 @@ public class Mapa {
 	private Map<Posicion, Celda> mapa =null; 
 	private int ancho;
 	private int largo;
-	private Map<Color, ArrayList<Ser>> seres; 
+	private Map<Color, ArrayList<Ser>> seres;
 	private ArrayList<Jugador> jugadores;
+	private Map<Color, ArrayList<EdificioDeRecurso>> edificiosDeGas;
+	private Map<Color, ArrayList<EdificioDeRecurso>> edificiosDeMineral;
+	
 
 	
 	private Mapa(int ancho, int largo,ArrayList<Jugador> jugadores){
 		this.mapa = new HashMap<Posicion, Celda>();
 		this.seres = new HashMap<Color, ArrayList<Ser>>();
+		this.edificiosDeGas = new HashMap<Color, ArrayList<EdificioDeRecurso>>();
+		this.edificiosDeMineral = new HashMap<Color, ArrayList<EdificioDeRecurso>>();
 		this.ancho = ancho;
 		this.largo = largo;
 		this.jugadores = jugadores;
@@ -28,38 +35,62 @@ public class Mapa {
 				mapa.put(p,c);
 			}
 		}
-		//inicializarMapa();
+		inicializarMapa();
 	}
 	
 	
 	private void inicializarMapa(){
 		Posicion pos = new Posicion(1,1);
-		//int clave = pos.hashCode();
-		/*Aunque el objeto pos de tipo Posicion sea distinto al ya creado dentro del mapa
-		 * el hashCode de ambos va a ser le mismo por tener las mismas coordenadas*/
 		Celda celda = mapa.get(pos);
-		VolcanGasVespeno volcan = new VolcanGasVespeno(pos);
-		/*celda.agregarFuenteRecurso(volcan);
+		VolcanGasVespeno volcan = new VolcanGasVespeno();
+		celda.agregarFuenteRecurso(volcan);
 		for(int i = 1;i<6;i++){
 			Posicion pos2 = new Posicion(i+2,1);
-			int clave2 = pos2.hashCode();
-			Celda celda2 = mapa.get(clave2);
-			Mineral mineral = new Mineral(pos2);
+			Celda celda2 = mapa.get(pos2);
+			Mineral mineral = new Mineral();
 			celda2.agregarFuenteRecurso(mineral);
-			}*/
+			}
 		
 	}
+	
 	public void ponerTerrestre(Posicion pos,Ser ser){
-		//int clave = pos.hashCode();
 		Celda celda = mapa.get(pos);
 		if (celda.ocupadoTerrestre()) System.out.println("La celda esta ocupada");
 		else celda.agregarSerTerrestre(ser);	
 		Color color = ser.color();
 		agregarSerDeColor(ser,color);
 	}
-		
+
+	public void ponerEdificioDeRecurso(Posicion pos,EdificioDeRecurso edificio){
+		Celda celda = mapa.get(pos);
+		if (celda.fuenteRecurso() == null) System.out.println("NO HAY RECURSO AHI"); //expection
+		else {
+			ponerTerrestre(pos,edificio);
+		}
+	}
+	
+	public void ponerEdificioGas(Posicion pos,EdificioDeRecurso edificio){
+		ponerEdificioDeRecurso(pos,edificio);
+		agregarEdificioDeRecursosDeColor(edificiosDeGas,edificio,edificio.color());
+	}
+	
+	public void ponerEdificioMineral(Posicion pos,EdificioDeRecurso edificio){
+		ponerEdificioDeRecurso(pos,edificio);
+		agregarEdificioDeRecursosDeColor(edificiosDeMineral,edificio,edificio.color());
+	}
+	
+	private void agregarEdificioDeRecursosDeColor(Map<Color, ArrayList<EdificioDeRecurso>> edificios,EdificioDeRecurso edificio, Color color) {
+		if (edificios.get(color) != null){
+			(edificios.get(color)).add(edificio);	
+		}
+		else{
+			ArrayList<EdificioDeRecurso> edificiosDeRecursoDeColor = new ArrayList<EdificioDeRecurso>();
+			edificiosDeRecursoDeColor.add(edificio);
+			edificios.put(color,edificiosDeRecursoDeColor);
+		}
+	}
+	
 	public void ponerAereo(Posicion pos,Ser ser) {
-		//int clave = pos.hashCode();
 		Celda celda = mapa.get(pos);
 		if (celda.ocupadoAerea()) System.out.println("La celda esta ocupada");
 		else celda.agregarSerAereo(ser);	
@@ -83,14 +114,17 @@ public class Mapa {
 			instancia = new Mapa(50,50,jugadores);
 		}
 	}
+	
 	public static Mapa getInstance(ArrayList<Jugador> jugadores) {
 		if (instancia == null)
 			createInstance(jugadores);
 		return instancia;
 	}
+	
 	public  Celda ContenidoFilaColumna(int fila, int columna) {
 		return mapa.get((new Posicion(fila,columna)));
 	}
+	
 	public ArrayList<Ser> seresDeJugador(Color color){
 		return (seres.get(color));
 	}
@@ -108,6 +142,7 @@ public class Mapa {
 		}
 		
 	}
+	
 	public void moverAerea(Ser unidadAMover, int xInicial, int yInicial,int xFinal,int yFinal) {
 		// aca deberia mover una unidad a la fila y columna que le pasan
 		//si no se puede (ocupado) deberia lanzar NoEsPosibleMoverException.
@@ -119,6 +154,14 @@ public class Mapa {
 			ponerAereo(new Posicion(xFinal,yFinal),unidadAMover);	
 			celda.desocuparAerea();
 		}
+	}
+	
+	public ArrayList<EdificioDeRecurso> edificioDeGas (Color color){
+		return (edificiosDeGas.get(color));
+	}
+	
+	public ArrayList<EdificioDeRecurso> edificioDeMineral (Color color){
+		return (edificiosDeMineral.get(color));
 	}
 }
 
