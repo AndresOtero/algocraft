d141bb27a56220d4fca496318d502ed0aafc19fc
alan <alanbenju@hotmail.com>
2015-06-15 01:33:51 -0300

    okey

--- algocraft/src/algo3/algocraft/edificios/EdificioEnConstruccion.java
+++ algocraft/src/algo3/algocraft/edificios/EdificioEnConstruccion.java
@@ -2,10 +2,17 @@ package algo3.algocraft.edificios;
 
 import algo3.algocraft.Color;
 import algo3.algocraft.Edificio;
+import algo3.algocraft.Mapa;
 
 public class EdificioEnConstruccion extends Edificio {
 	public EdificioEnConstruccion(Color colorJugador){
 		this.vida = 50;
 		this.color=colorJugador;
+	}
+
+	@Override
+	public void agregarseAMapa(Mapa mapa) {
+		// TODO Auto-generated method stub
+		
 	}
 }
