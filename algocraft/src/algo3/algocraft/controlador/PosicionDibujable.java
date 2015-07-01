ca915289446d026445aa6374135dc7121fd1b794
Federico Brasburg <fede_yo_soy@hotmail.com>
2015-07-01 18:22:43 -0300

    Subo controlador final, fijense mas errores ( y subo una clase auxiliar que implemente)

---
+++ algocraft/src/algo3/algocraft/controlador/PosicionDibujable.java
@@ -0,0 +1,17 @@
+package algo3.algocraft.controlador;
+
+public class PosicionDibujable {
+	private double X;
+	private double Y;
+		public PosicionDibujable(double x, double y){
+		X =x;
+		Y = y;
+	}
+	
+	public double y(){
+		return Y;
+	}
+	public double x(){
+		return X;
+	}
+}
